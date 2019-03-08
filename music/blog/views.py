ae20c07e007027eefa7d1449bde19a66c6376966
xd <xmandunn@gmail.com>
2019-03-08 11:44:54 -0500

    added some crud

--- music/blog/views.py
+++ music/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def blog(request):
@@ -17,11 +18,40 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
-class PostDetailView(ListView):
+class PostDetailView(DetailView):
     model = Post
-    template_name = 'pages/post_detail.html'
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
