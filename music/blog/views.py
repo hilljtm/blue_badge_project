9a26f0bf7719fc35dd9431ce73e521de2ef10504
xd <xmandunn@gmail.com>
2019-03-08 10:17:13 -0500

    small changes

--- music/blog/views.py
+++ music/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -7,3 +8,20 @@ def blog(request):
         'posts': Post.objects.all()
     }
     return render(request, 'pages/blog.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'pages/blog.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(ListView):
+    model = Post
+    template_name = 'pages/post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
