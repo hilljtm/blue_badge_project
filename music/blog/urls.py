344def14b271672239be17cc1c9b52143267727e
xd <xmandunn@gmail.com>
2019-03-11 14:46:39 -0400

    finished up comments, del and edit post links, see all posts by a single user, links to that and detailed view pages working

--- music/blog/urls.py
+++ music/blog/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blogPage'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='userPosts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='postDetail'),
     path('post/new/', PostCreateView.as_view(), name='postCreate'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='postUpdate'),
